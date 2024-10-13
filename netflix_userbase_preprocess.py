60c61e641d05be388156f65034603e5396ae5856
koeyhi <104114941+koeyhi@users.noreply.github.com>
2024-10-13 13:06:20 +0000

    컬럼 생성 및 날짜 형식 변경 함수화

--- netflix_userbase_preprocess.py
+++ netflix_userbase_preprocess.py
@@ -2,39 +2,50 @@ import pandas as pd
 
 DATA_PATH = "data/"
 
-# 날짜 형식 변경
-# df = pd.read_csv(f"{DATA_PATH}Netflix Userbase.csv")
-# df["Join Date"] = pd.to_datetime(df["Join Date"], format="%d-%m-%y").dt.strftime("%Y-%m-%d")
-# df["Last Payment Date"] = pd.to_datetime(df["Last Payment Date"], format="%d-%m-%y").dt.strftime("%Y-%m-%d")
-# df.to_csv("C:/Users/kwon3/Desktop/converted_file.csv", index=False)
-
-df = pd.read_csv(f"{DATA_PATH}converted_file.csv")
+df = pd.read_csv(f"{DATA_PATH}Netflix Userbase.csv")
 country_type_fee_df = pd.read_csv(f"{DATA_PATH}Netflix subscription fee Dec-2021.csv")
 revenue_df = pd.read_csv(f"{DATA_PATH}netflix_revenue_updated.csv")
 
-subscription_costs = []
 
-for i, user in df.iterrows():
-    user_country = user["Country"]
-    user_subscription = user["Subscription Type"]
+# 국가, 등급별 구독금액 컬럼 생성
+def subscription_cost():
+    subscription_costs = []
+    for i, user in df.iterrows():
+        user_country = user["Country"]
+        user_subscription = user["Subscription Type"]
 
-    country_cost = country_type_fee_df[country_type_fee_df["Country"] == user_country]
+        country_cost = country_type_fee_df[
+            country_type_fee_df["Country"] == user_country
+        ]
 
-    if not country_cost.empty:
-        if user_subscription == "Basic":
-            subscription_costs.append(
-                country_cost["Cost Per Month - Basic ($)"].values[0]
-            )
-        elif user_subscription == "Standard":
-            subscription_costs.append(
-                country_cost["Cost Per Month - Standard ($)"].values[0]
-            )
-        elif user_subscription == "Premium":
-            subscription_costs.append(
-                country_cost["Cost Per Month - Premium ($)"].values[0]
-            )
-    else:
-        subscription_costs.append("UNK")
+        if not country_cost.empty:
+            if user_subscription == "Basic":
+                subscription_costs.append(
+                    country_cost["Cost Per Month - Basic ($)"].values[0]
+                )
+            elif user_subscription == "Standard":
+                subscription_costs.append(
+                    country_cost["Cost Per Month - Standard ($)"].values[0]
+                )
+            elif user_subscription == "Premium":
+                subscription_costs.append(
+                    country_cost["Cost Per Month - Premium ($)"].values[0]
+                )
+        else:
+            subscription_costs.append("UNK")
 
-df["Subscription Cost"] = subscription_costs
-df.to_csv("netflix_userbase_preprocessed.csv")
+    return subscription_costs
+
+
+# 날짜 형식 변경
+def to_datetime():
+    df["Join Date"] = pd.to_datetime(df["Join Date"], dayfirst=True, format="%d-%m-%y")
+    df["Last Payment Date"] = pd.to_datetime(
+        df["Last Payment Date"], dayfirst=True, format="%d-%m-%y"
+    )
+
+
+df["Subscription Cost"] = subscription_cost()
+to_datetime()
+
+df.to_csv(f"{DATA_PATH}netflix_userbase_preprocessed.csv")
