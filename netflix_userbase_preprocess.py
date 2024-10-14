b69568e2067f54485d1607be32cf4ff8be4b284f
kangnahyun <gnh1210@naver.com>
2024-10-14 09:25:55 +0900

    연령대 범주화 및 불필요 열 삭제

--- netflix_userbase_preprocess.py
+++ netflix_userbase_preprocess.py
@@ -48,4 +48,22 @@ def to_datetime():
 df["Subscription Cost"] = subscription_cost()
 to_datetime()
 
+#연령대 분류
+bins = [10, 20, 30, 40, 50, float('inf')]
+labels = ['10대', '20대', '30대', '40대', '50대 이상']
+
+bins2 = [18, 35, 50, 65, float('inf')]
+labels2 = ['청년층', '중년층', '장년층', '노년층']
+
+#연령대 범주화
+Age_Group = pd.cut(df['Age'], bins=bins, labels=labels, right=False)
+Age_Group2 = pd.cut(df['Age'], bins=bins2, labels=labels2, right=False)
+
+#M-R, P-D 열 삭제
+df = df.drop(['Monthly Revenue', 'Plan Duration'], axis=1)
+
+#연령대 열 삽입
+df.insert(6, '나이대',Age_Group)
+df.insert(7, '연령대',Age_Group2)
+
 df.to_csv(f"{DATA_PATH}netflix_userbase_preprocessed.csv")
